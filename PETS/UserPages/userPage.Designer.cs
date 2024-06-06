6a6588291fc6c0c7c2b60a8cbb106cc81ac63361
Robertas <Robertas@DESKTOP-PSFI987>
2024-06-06 20:32:28 +0300

    Added userPage labels and buttons

--- PETS/UserPages/userPage.Designer.cs
+++ PETS/UserPages/userPage.Designer.cs
@@ -28,25 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.userInformationLabel = new System.Windows.Forms.Label();
+            this.logoutBtn = new System.Windows.Forms.Button();
+            this.petsNameLabel = new System.Windows.Forms.Label();
+            this.petsAgeLabel = new System.Windows.Forms.Label();
+            this.petsChipLabel = new System.Windows.Forms.Label();
+            this.petsVaccineDateLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.userLabel = new System.Windows.Forms.Label();
+            this.userAdressLabel = new System.Windows.Forms.Label();
+            this.userEmailLabel = new System.Windows.Forms.Label();
+            this.petsNextVaccineDateLabel = new System.Windows.Forms.Label();
+            this.userEditBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // userInformationLabel
+            // 
+            this.userInformationLabel.AccessibleName = "userInformationLabel";
+            this.userInformationLabel.AutoSize = true;
+            this.userInformationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInformationLabel.Location = new System.Drawing.Point(27, 26);
+            this.userInformationLabel.Name = "userInformationLabel";
+            this.userInformationLabel.Size = new System.Drawing.Size(194, 25);
+            this.userInformationLabel.TabIndex = 0;
+            this.userInformationLabel.Text = "userInformationLabel";
+            // 
+            // logoutBtn
+            // 
+            this.logoutBtn.AccessibleName = "logoutBtn";
+            this.logoutBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutBtn.Location = new System.Drawing.Point(633, 26);
+            this.logoutBtn.Name = "logoutBtn";
+            this.logoutBtn.Size = new System.Drawing.Size(121, 35);
+            this.logoutBtn.TabIndex = 1;
+            this.logoutBtn.Text = "Atsijungti";
+            this.logoutBtn.UseVisualStyleBackColor = true;
+            // 
+            // petsNameLabel
+            // 
+            this.petsNameLabel.AccessibleName = "petsNameLabel";
+            this.petsNameLabel.AutoSize = true;
+            this.petsNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsNameLabel.Location = new System.Drawing.Point(404, 133);
+            this.petsNameLabel.Name = "petsNameLabel";
+            this.petsNameLabel.Size = new System.Drawing.Size(149, 25);
+            this.petsNameLabel.TabIndex = 2;
+            this.petsNameLabel.Text = "petsNameLabel";
+            // 
+            // petsAgeLabel
+            // 
+            this.petsAgeLabel.AccessibleName = "petsAgeLabel";
+            this.petsAgeLabel.AutoSize = true;
+            this.petsAgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsAgeLabel.Location = new System.Drawing.Point(404, 177);
+            this.petsAgeLabel.Name = "petsAgeLabel";
+            this.petsAgeLabel.Size = new System.Drawing.Size(133, 25);
+            this.petsAgeLabel.TabIndex = 3;
+            this.petsAgeLabel.Text = "petsAgeLabel";
+            // 
+            // petsChipLabel
+            // 
+            this.petsChipLabel.AccessibleName = "petsChipLabel";
+            this.petsChipLabel.AutoSize = true;
+            this.petsChipLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsChipLabel.Location = new System.Drawing.Point(404, 220);
+            this.petsChipLabel.Name = "petsChipLabel";
+            this.petsChipLabel.Size = new System.Drawing.Size(138, 25);
+            this.petsChipLabel.TabIndex = 4;
+            this.petsChipLabel.Text = "petsChipLabel";
+            // 
+            // petsVaccineDateLabel
+            // 
+            this.petsVaccineDateLabel.AccessibleName = "petsVaccineDateLabel";
+            this.petsVaccineDateLabel.AutoSize = true;
+            this.petsVaccineDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsVaccineDateLabel.Location = new System.Drawing.Point(404, 263);
+            this.petsVaccineDateLabel.Name = "petsVaccineDateLabel";
+            this.petsVaccineDateLabel.Size = new System.Drawing.Size(209, 25);
+            this.petsVaccineDateLabel.TabIndex = 5;
+            this.petsVaccineDateLabel.Text = "petsVaccineDateLabel";
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "petsLabel";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(404, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Jūsų gyvūnas:";
             // 
             // userLabel
             // 
             this.userLabel.AccessibleName = "userLabel";
             this.userLabel.AutoSize = true;
-            this.userLabel.Location = new System.Drawing.Point(316, 188);
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(27, 84);
             this.userLabel.Name = "userLabel";
-            this.userLabel.Size = new System.Drawing.Size(67, 16);
-            this.userLabel.TabIndex = 0;
-            this.userLabel.Text = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(159, 25);
+            this.userLabel.TabIndex = 7;
+            this.userLabel.Text = "Jūsų informacija:";
+            // 
+            // userAdressLabel
+            // 
+            this.userAdressLabel.AccessibleName = "userAdressLabel";
+            this.userAdressLabel.AutoSize = true;
+            this.userAdressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userAdressLabel.Location = new System.Drawing.Point(27, 177);
+            this.userAdressLabel.Name = "userAdressLabel";
+            this.userAdressLabel.Size = new System.Drawing.Size(160, 25);
+            this.userAdressLabel.TabIndex = 8;
+            this.userAdressLabel.Text = "userAdressLabel";
+            // 
+            // userEmailLabel
+            // 
+            this.userEmailLabel.AccessibleName = "userEmailLabel";
+            this.userEmailLabel.AutoSize = true;
+            this.userEmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userEmailLabel.Location = new System.Drawing.Point(27, 133);
+            this.userEmailLabel.Name = "userEmailLabel";
+            this.userEmailLabel.Size = new System.Drawing.Size(146, 25);
+            this.userEmailLabel.TabIndex = 9;
+            this.userEmailLabel.Text = "userEmailLabel";
+            // 
+            // petsNextVaccineDateLabel
+            // 
+            this.petsNextVaccineDateLabel.AccessibleName = "petsNextVaccineDateLabel";
+            this.petsNextVaccineDateLabel.AutoSize = true;
+            this.petsNextVaccineDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsNextVaccineDateLabel.Location = new System.Drawing.Point(404, 305);
+            this.petsNextVaccineDateLabel.Name = "petsNextVaccineDateLabel";
+            this.petsNextVaccineDateLabel.Size = new System.Drawing.Size(249, 25);
+            this.petsNextVaccineDateLabel.TabIndex = 10;
+            this.petsNextVaccineDateLabel.Text = "petsNextVaccineDateLabel";
+            // 
+            // userEditBtn
+            // 
+            this.userEditBtn.AccessibleName = "userEditBtn";
+            this.userEditBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userEditBtn.Location = new System.Drawing.Point(44, 234);
+            this.userEditBtn.Name = "userEditBtn";
+            this.userEditBtn.Size = new System.Drawing.Size(111, 39);
+            this.userEditBtn.TabIndex = 11;
+            this.userEditBtn.Text = "Redaguoti";
+            this.userEditBtn.UseVisualStyleBackColor = true;
             // 
             // userPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userEditBtn);
+            this.Controls.Add(this.petsNextVaccineDateLabel);
+            this.Controls.Add(this.userEmailLabel);
+            this.Controls.Add(this.userAdressLabel);
             this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.petsVaccineDateLabel);
+            this.Controls.Add(this.petsChipLabel);
+            this.Controls.Add(this.petsAgeLabel);
+            this.Controls.Add(this.petsNameLabel);
+            this.Controls.Add(this.logoutBtn);
+            this.Controls.Add(this.userInformationLabel);
             this.Name = "userPage";
             this.Text = "userPage";
             this.Load += new System.EventHandler(this.userPage_Load);
@@ -57,6 +201,17 @@
 
         #endregion
 
+        private System.Windows.Forms.Label userInformationLabel;
+        private System.Windows.Forms.Button logoutBtn;
+        private System.Windows.Forms.Label petsNameLabel;
+        private System.Windows.Forms.Label petsAgeLabel;
+        private System.Windows.Forms.Label petsChipLabel;
+        private System.Windows.Forms.Label petsVaccineDateLabel;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label userAdressLabel;
+        private System.Windows.Forms.Label userEmailLabel;
+        private System.Windows.Forms.Label petsNextVaccineDateLabel;
+        private System.Windows.Forms.Button userEditBtn;
     }
 }
--- PETS/UserPages/userPage.cs
+++ PETS/UserPages/userPage.cs
@@ -24,7 +24,7 @@ namespace PETS.UserPages
 
         private void DisplayUserInfo()
         {
-            userLabel.Text = _userInfo.FirstName;
+            //userLabel.Text = _userInfo.FirstName;
         }
 
         private void userPage_Load(object sender, EventArgs e)
